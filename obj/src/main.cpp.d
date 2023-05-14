7251a3911817081d14aecf32aca6686b990a6492
Eugene109 <79175776+Eugene109@users.noreply.github.com>
2023-05-13 22:05:08 -0400

    first commit

---
+++ obj/src/main.cpp.d
@@ -0,0 +1,21 @@
+obj/src/main.cpp.bc: src\main.cpp C:\Users\Eugene\CEdev\include\graphx.h \
+  C:\Users\Eugene\CEdev\include\stdint.h \
+  C:\Users\Eugene\CEdev\include\cdefs.h \
+  C:\Users\Eugene\CEdev\include\stdlib.h \
+  C:\Users\Eugene\CEdev\include\stdbool.h \
+  C:\Users\Eugene\CEdev\include\sys\timers.h \
+  C:\Users\Eugene\CEdev\include\sys\util.h \
+  C:\Users\Eugene\CEdev\include\ti\getcsc.h \
+  C:\Users\Eugene\CEdev\include\c++\cassert \
+  C:\Users\Eugene\CEdev\include\assert.h \
+  C:\Users\Eugene\CEdev\include\c++\cstring \
+  C:\Users\Eugene\CEdev\include\string.h \
+  C:\Users\Eugene\CEdev\include\stdio.h \
+  C:\Users\Eugene\CEdev\include\stdarg.h \
+  C:\Users\Eugene\CEdev\include\time.h src\matrixMath.h \
+  C:\Users\Eugene\CEdev\include\c++\cmath \
+  C:\Users\Eugene\CEdev\include\math.h \
+  C:\Users\Eugene\CEdev\include\c++\type_traits \
+  C:\Users\Eugene\CEdev\include\c++\__config \
+  C:\Users\Eugene\CEdev\include\c++\cstddef \
+  C:\Users\Eugene\CEdev\include\stddef.h src\vectorMath.h
